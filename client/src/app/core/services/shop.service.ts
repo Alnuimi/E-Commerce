d8245cfe5549c2e54d1e9d740d9ed870556189e0
Alnuimi <italnuimi@gmail.com>
2024-11-25 19:18:34 +0300

    Configruation Routing

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -10,43 +10,46 @@ import { shopParams } from '../../shared/models/shopParams';
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/'
   private http = inject(HttpClient);
-  types: string[] =[];
-  brands: string[] =[];
+  types: string[]  = [];
+  brands: string[] = [];
+
 
   getProducts(shopParams:shopParams){
     let param = new HttpParams();
 
     if(shopParams.brands && shopParams.brands.length>0){
    
-     param= param.append('brands',shopParams.brands.join(','));
+     param= param.append('brands', shopParams.brands.join(','));
     }
-    if(shopParams.types && shopParams.types.length>0){
-     param= param.append('types',shopParams.types.join(','));
+    if(shopParams.types && shopParams.types.length > 0){
+     param= param.append('types', shopParams.types.join(','));
     }
     if(shopParams.sort){
-      param = param.append("sort",shopParams.sort);
+      param = param.append("sort" , shopParams.sort);
     }
 
     if(shopParams.search){
-      param = param.append("search",shopParams.search);
+      param = param.append("search" , shopParams.search);
     }
-    param= param.append('pageSize',shopParams.pageSize);
-    param= param.append('pageIndex',shopParams.pageNumber);
+    param = param.append('pageSize' , shopParams.pageSize);
+    param = param.append('pageIndex' , shopParams.pageNumber);
    
-   return  this.http.get<Pagination<Product>>(this.baseUrl + 'product',{params:param})
+   return  this.http.get<Pagination<Product>>(this.baseUrl + 'product' ,{params:param})
   }
-
+  getProductByID(id:number){
+   return this.http.get<Product>(this.baseUrl + 'product/' + id);
+  }
   getBrands(){
     if(this.brands.length>0) return;
-    return this.http.get<string[]>(this.baseUrl+'product/brands').subscribe({
-      next:response => this.brands=response
+    return this.http.get<string[]>(this.baseUrl + 'product/brands').subscribe({
+      next: response => this.brands = response
     })
   }
 
   getTypes(){
     if(this.types.length>0) return;
-    return this.http.get<string[]>(this.baseUrl+'product/types').subscribe({
-      next:response=> this.types=response
+    return this.http.get<string[]>(this.baseUrl + 'product/types').subscribe({
+      next: response => this.types = response
     })
   }
 }
